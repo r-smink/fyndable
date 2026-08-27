9e16339c4abc89590f55af72333abe9fbed0d80e
Rick Smink <rick@nextbuzz.nl>
2026-08-27 12:05:42 +0200

    v3 app

--- android-app/app/src/main/java/com/fyndable/mobile/ui/login/LoginScreen.kt
+++ android-app/app/src/main/java/com/fyndable/mobile/ui/login/LoginScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
--- android-app/app/src/main/java/com/fyndable/mobile/ui/navigation/AppNavigation.kt
+++ android-app/app/src/main/java/com/fyndable/mobile/ui/navigation/AppNavigation.kt
@@ -1,6 +1,7 @@
 package com.fyndable.mobile.ui.navigation
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
